c0039c7c139b48e95b59d50fddc1cdaacce841c1
victor-430 <oluwatobivictor430@gmail.com>
2026-05-21 07:42:48 +0100

    fix(loginauth context): remove login auth context in app which causes uneccessary checks
    - middleware now handles role checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/index";
 import { Toaster } from "./components/ui/sonner";
-import { AuthProvider } from "./context/loginAuthContext";
+// import { AuthProvider } from "./context/loginAuthContext";
 
 function App() {
   return (
     
-    <AuthProvider>
-
+<>
       <RouterProvider router={router} />
       <Toaster />
-    </AuthProvider>
+
+</>
+    
     
   );
 }
