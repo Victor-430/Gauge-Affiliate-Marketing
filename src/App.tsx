782819b54286ab3393575b184dded678d152490e
victor-430 <oluwatobivictor430@gmail.com>
2026-05-21 08:34:27 +0100

    fix:(login auth context): use login auth context to prevent isolated auth state per component call

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/index";
 import { Toaster } from "./components/ui/sonner";
-// import { AuthProvider } from "./context/loginAuthContext";
+import { AuthProvider } from "./context/loginAuthContext";
 
 function App() {
   return (
     
-<>
+    <AuthProvider>
       <RouterProvider router={router} />
       <Toaster />
-
-</>
-    
+    </AuthProvider>
     
   );
 }
